451ba1d62d54f08757c46f1290c9407585dd4ecd
Alex Hutchings <alexhutchings4@gmail.com>
2018-12-17 15:40:38 +0000

    Added chapter 8

---
+++ chapter8/dictionary.rb
@@ -0,0 +1,7 @@
+dict = {
+  :bear => "a creature that fishes in the river for salmon.",
+  :river => "a body of water that contains salmon, and sometimes bears.",
+  :salmon => "a fish, sometimes in a river, sometimes in a bear, and sometimes in both."
+}
+
+puts dict[:bear]
